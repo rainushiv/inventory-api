027d0fe6158091fbdf516fff76c0c138452d8dd3
rainushiv <shivpreet567@gmail.com>
2026-03-28 14:25:27 -0400

    feat: Add location pydantic model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .unit import UnitCreate,UnitResponse
 from .variant import VariantCreate,VariantResponse
 from .device_model import DeviceModelCreate, DeviceModelResponse
 from .product import ProductCreate, ProductResponse
+from .location import LocationCreate,LocationResponse 
