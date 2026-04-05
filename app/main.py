a553e4f19f6be3d6bfa7c7e4860bfaf81f17d7e7
rainushiv <shivpreet567@gmail.com>
2026-04-05 16:00:53 -0400

    Feat: Added rotating file logger and stream logger

--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
-from routers import product
-from routers import unit 
-from InventoryBackend.app.routers import variant 
-from routers import model
-from routers import variant
+from .routers import product
+from .routers import unit 
+from .routers import model
+from .routers import variant
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
-from db import lifespan
+import logging
+from logging.handlers import RotatingFileHandler
+from .db import lifespan
+
+
+logging.basicConfig( 
+        level=logging.INFO,
+        format='%(filename)s:%(asctime)s - %(message)s - %(levelname)s', datefmt='%m/%d/%Y %I:%M:%S %p'
+      )
+
+logger = logging.getLogger(__name__)
+
+"""Add File logger rotator """
+rotate_file_handler = RotatingFileHandler(filename="logs/log.out",maxBytes=50,backupCount=5)
+rotate_file_handler.setLevel(logging.ERROR)
+format= logging.Formatter('%(filename)s:%(asctime)s - %(message)s -  %(levelname)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+rotate_file_handler.setFormatter(format)
+logger.addHandler(rotate_file_handler)
+
 
 load_dotenv()
+
 
 app = FastAPI(
     title="InventoryBackend",
@@ -22,7 +40,7 @@ app.include_router(unit.router)
 
 @app.get("/")
 async def root():
-    
+    logger.info("HEKKI IS THIS THING ON")
     return {"message": "Hello World"}
 
 @app.get("/health")
--- app/routers/model.py
+++ app/routers/model.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-
+import logging
 router = APIRouter()
-
+logger = logging.getLogger(__name__)
 @router.get("/model", tags=["model"])
 async def get_models():
+    logger.info("This model happened ")
     return [{"Iphone": "Iphone 13"},{"Samsung": "Samsung S22"}]
 
 @router.post("/model", tags=["model"])
