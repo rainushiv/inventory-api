e220b6dd2e234f1ea4145a3dd75493f1b0e9debf
rainushiv <shivpreet567@gmail.com>
2026-04-07 11:15:39 -0400

    Refactor: Updated .gitignore

--- app/routers/model.py
+++ app/routers/model.py
@@ -5,6 +5,7 @@ logger = logging.getLogger("my_logger")
 @router.get("/model", tags=["model"])
 async def get_models():
     logger.info("Called to get all models")
+    logger.error("Called to get all models")
     return [{"Iphone": "Iphone 13"},{"Samsung": "Samsung S22"}]
 
 @router.post("/model", tags=["model"])
